0421f7364fabb254e000529a3e7f533f8a2e748e
Carlos Giovano <carlosgiovano@outlook.com>
2020-04-19 14:06:32 -0300

    monogame Spline Test

---
+++ MonoGameSplineTest/Game1.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace MonoGameSplineTest
+{
+    struct Point2D
+    {
+        public int x;
+        public int y;
+
+        public Point2D(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Spline path;
+
+        List<Point2D> points;
+        Texture2D lineTex;
+        Texture2D cpTex;
+        Spline spline;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var p0 = new Point2D();
+            p0.x = 100;
+            p0.y = 100;
+
+            var p1 = new Point2D();
+            p1.x = 200;
+            p1.y = 200;
+
+            var p2 = new Point2D();
+            p2.x = 300;
+            p2.y = 300;
+
+            var p3 = new Point2D();
+            p3.x = 400;
+            p3.y = 400;
+
+            var p4 = new Point2D();
+            p4.x = 500;
+            p4.y = 100;
+
+            var p5 = new Point2D();
+            p5.x = 600;
+            p5.y = 100;
+
+            //var p0 = new Point2D();
+            //p0.x = 100;
+            //p0.y = 100;
+
+            points = new List<Point2D>();
+            points.Add(p0);
+            points.Add(p1);
+            points.Add(p2);
+            points.Add(p3);
+            points.Add(p4);
+            points.Add(p5);
+
+            // TODO: use this.Content to load your game content here
+            cpTex = Content.Load<Texture2D>("ControlPoint");
+            lineTex = Content.Load<Texture2D>("LinePoint");
+
+
+            spline = new Spline(points.ToArray(), cpTex, lineTex);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            spline.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            spline.Draw(spriteBatch, GraphicsDevice);
+            
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoGameSplineTest/Spline.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameSplineTest
+{
+    class Spline
+    {
+        private Point2D[] _controlPointList;
+        private Texture2D _pixelControlPointTexture, _pixelLineTexture;
+        private List<Vector2> _controlPoints;
+        private int _selectedIndex = 0;
+
+        private List<Vector2[]> _splinePoints;
+
+        KeyboardState ks1, ks2;
+
+        public Spline(Point2D[] pointList, Texture2D controlPointTex, Texture2D lineTexture)
+        {
+            _controlPointList = pointList;
+            _pixelControlPointTexture = controlPointTex;
+            _pixelLineTexture = lineTexture;
+            _splinePoints = new List<Vector2[]>();
+            
+        }
+
+        //Get the catmull-rom spline
+        private void GetCatmullRomSpline()
+        {
+            //For Catmull-Rom spline we need four control points to calculate the curve
+
+            int numPoints = 4;
+
+            if (_controlPointList.Length < 4)
+                throw new ArgumentException("The Catmull-Rom spline needs at least 4 control points");
+
+            for (int i = 0; i < _controlPointList.Length - 3; i++)
+            {
+                _controlPoints = new List<Vector2>();
+
+                for (int j = 0; j < 20; j++)
+                {
+                    _controlPoints.Add(GetPointOnCurvePosition(_controlPointList[i], _controlPointList[i + 1], _controlPointList[i + 2], _controlPointList[i + 3], .05f*j)); //(1f / numPoints) * j)
+                }
+
+                _splinePoints.Add(_controlPoints.ToArray());
+            }
+
+            _controlPoints.Clear();
+        }
+
+        //Calculate the line
+        private Vector2 GetPointOnCurvePosition(Point2D p0, Point2D p1, Point2D p2, Point2D p3, float t)
+        {
+            Vector2 ret = new Vector2();
+
+            float t2 = t * t;
+            float t3 = t2 * t;
+
+            ret.X = 0.5f * ((2.0f * p1.x) +
+            (-p0.x + p2.x) * t +
+            (2.0f * p0.x - 5.0f * p1.x + 4 * p2.x - p3.x) * t2 +
+            (-p0.x + 3.0f * p1.x - 3.0f * p2.x + p3.x) * t3);
+
+            ret.Y = 0.5f * ((2.0f * p1.y) +
+            (-p0.y + p2.y) * t +
+            (2.0f * p0.y - 5.0f * p1.y + 4 * p2.y - p3.y) * t2 +
+            (-p0.y + 3.0f * p1.y - 3.0f * p2.y + p3.y) * t3);
+
+            return (ret);
+        }
+
+
+        public void Update(GameTime gameTime)
+        {
+            ks1 = Keyboard.GetState();
+
+            if (ks1.IsKeyDown(Keys.S) && ks2.IsKeyUp(Keys.S) && _selectedIndex < (_controlPointList.Length - 1))
+            {
+                _selectedIndex++;
+            }
+            if (ks1.IsKeyDown(Keys.A) && ks2.IsKeyUp(Keys.A) && _selectedIndex > 0)
+            {
+                _selectedIndex--;
+            }
+
+            ks2 = ks1;
+
+
+            if (ks1.IsKeyDown(Keys.Up))
+            {
+                _controlPointList[_selectedIndex].y -= 1;
+            }
+            if (ks1.IsKeyDown(Keys.Down))
+            {
+                _controlPointList[_selectedIndex].y += 1;
+            }
+            if (ks1.IsKeyDown(Keys.Right))
+            {
+                _controlPointList[_selectedIndex].x += 1;
+            }
+            if (ks1.IsKeyDown(Keys.Left))
+            {
+                _controlPointList[_selectedIndex].x -= 1;
+            }
+
+            GetCatmullRomSpline();
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GraphicsDevice gd)
+        {
+            gd.Clear(Color.Black);
+            spriteBatch.Begin();
+            
+            for (int i = 0; i < _splinePoints.Count; i++)
+            {
+                for (int j = 0; j < _splinePoints[i].Length; j++)
+                {
+                    spriteBatch.Draw(_pixelLineTexture, _splinePoints[i][j], Color.Yellow);
+                }
+            }
+
+            for (int i = 0; i < _controlPointList.Length; i++)
+            {
+                if (_selectedIndex == i)
+                    spriteBatch.Draw(_pixelLineTexture, new Vector2(_controlPointList[i].x, _controlPointList[i].y), null, Color.CadetBlue, 0, Vector2.Zero, 3f, SpriteEffects.None, 0);
+                else
+                    spriteBatch.Draw(_pixelLineTexture, new Vector2(_controlPointList[i].x, _controlPointList[i].y), null, Color.Red, 0, Vector2.Zero, 3f, SpriteEffects.None, 0);
+            }
+
+            spriteBatch.End();
+
+            for (var i = 0; i < _splinePoints.Count; i++)
+            {
+                _splinePoints.RemoveAt(i);
+            }
+        }
+    }
+}
